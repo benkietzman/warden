1e5c5f04e08bee2009cd0c74b1afb43bf142e4bc
Ben Kietzman <ben@kietzman.org>
2021-04-16 14:27:59 -0500

    Added the framework for the Authn class.

---
+++ module/include/Authn.cpp
@@ -0,0 +1,44 @@
+// vim: syntax=cpp
+// vim600: fdm=marker
+/* -*- c++ -*- */
+///////////////////////////////////////////
+// Authentication
+// -------------------------------------
+// file       : Authn.cpp
+// author     : Ben Kietzman
+// begin      : 2021-04-16
+// copyright  : kietzman.org
+// email      : ben@kietzman.org
+///////////////////////////////////////////
+
+/**************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+**************************************************************************/
+
+/*! \file Authn
+* \brief Authn Class
+*/  
+// {{{ includes
+#include "Authn"
+// }}}
+extern "C++"
+{ 
+  namespace warden
+  {
+    // {{{ Authn()
+    Authn::Authn()
+    {
+    }
+    // }}}
+    // {{{ ~Authn()
+    Authn::~Authn()
+    {
+    }
+    // }}}
+  }
+}

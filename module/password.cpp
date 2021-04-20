98930a617399c588422256ee4813030d5301e808
Ben Kietzman <ben@kietzman.org>
2021-04-20 15:15:46 -0500

    Saved work.

--- module/password.cpp
+++ module/password.cpp
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
   if (getline(cin, strJson))
   {
     list<string> keys;
-    string strSubError;
+    string strApplication, strPassword, strSubError, strType, strUser;
     stringstream ssCurrent;
     time_t CCurrent;
     Json *ptData;
@@ -105,225 +105,200 @@ int main(int argc, char *argv[])
     delete ptData;
     keys.clear();
     // }}}
-    if (ptJson->m.find("Function") != ptJson->m.end() && !ptJson->m["Function"]->v.empty())
+    if (ptJson->m.find("Application") != ptJson->m.end() && !ptJson->m["Application"]->v.empty())
     {
-      string strApplication, strFunction = ptJson->m["Function"]->v, strPassword, strType, strUser;
-      if (ptJson->m.find("Application") != ptJson->m.end() && !ptJson->m["Application"]->v.empty())
+      strApplication = ptJson->m["Application"]->v;
+    }
+    if (ptJson->m.find("Password") != ptJson->m.end() && !ptJson->m["Password"]->v.empty())
+    {
+      strPassword = ptJson->m["Password"]->v;
+    }
+    if (ptJson->m.find("Type") != ptJson->m.end() && !ptJson->m["Type"]->v.empty())
+    {
+      strType = ptJson->m["Type"]->v;
+    }
+    if (ptJson->m.find("User") != ptJson->m.end() && !ptJson->m["User"]->v.empty())
+    {
+      strUser = ptJson->m["User"]->v;
+    }
+    if (!strApplication.empty())
+    {
+      string strSubError;
+      ptData = new Json;
+      keys.push_back(strApplication);
+      keys.push_back(strUser);
+      if (pStorage->retrieve(keys, ptData, strSubError) && (strType.empty() || (ptData->m.find("Type") != ptData->m.end() && ptData->m["Type"]->v == strType)) && ptData->m.find("Password") != ptData->m.end() && ptData->m["Password"]->v == strPassword)
       {
-        strApplication = ptJson->m["Application"]->v;
+        bProcessed = true;
       }
-      if (ptJson->m.find("Password") != ptJson->m.end() && !ptJson->m["Password"]->v.empty())
+      delete ptData;
+      keys.clear();
+      if (!bProcessed)
       {
-        strPassword = ptJson->m["Password"]->v;
-      }
-      if (ptJson->m.find("Type") != ptJson->m.end() && !ptJson->m["Type"]->v.empty())
-      {
-        strType = ptJson->m["Type"]->v;
-      }
-      if (ptJson->m.find("User") != ptJson->m.end() && !ptJson->m["User"]->v.empty())
-      {
-        strUser = ptJson->m["User"]->v;
-      }
-      // {{{ login
-      if (strFunction == "login")
-      {
+        list<string> in, out;
+        ServiceJunction junction(strError);
+        junction.setApplication("Warden");
         ptData = new Json;
-        keys.push_back(strUser);
-        if (pStorage->retrieve(keys, ptData, strSubError) && ptData->m.find("Password") != ptData->m.end() && ptData->m["Password"]->v == strPassword)
-        {
-          bProcessed = true;
-        }
+        ptData->insert("Service", "password");
+        ptData->insert("Function", "verify");
+        ptData->insert("Application", strApplication);
+        ptData->insert("User", strUser);
+        ptData->insert("Password", strPassword);
+        ptData->insert("Type", strType);
+        in.push_back(ptData->json(strJson));
         delete ptData;
-        keys.clear();
-        if (!bProcessed)
+        if (junction.request(in, out, strError))
         {
-          list<string> subKeys;
-          Json *ptConf = new Json;
-          Warden warden("Central", strUnix, strError);
-          subKeys.push_back("conf");
-          if (warden.vaultRetrieve(subKeys, ptConf, strError))
+          if (!out.empty())
           {
-            if (ptConf->m.find("Database") != ptConf->m.end() && !ptConf->m["Database"]->v.empty())
+            Json *ptStatus = new Json(out.front());
+            if (ptStatus->m.find("Status") != ptStatus->m.end() && ptStatus->m["Status"]->v == "okay")
             {
-              if (ptConf->m.find("Database Password") != ptConf->m.end() && !ptConf->m["Database Password"]->v.empty())
+              bProcessed = true;
+              keys.push_back(strApplication);
+              keys.push_back(strUser);
+              ptData = new Json;
+              ptData->insert("_modified", ssCurrent.str(), 'n');
+              if (!strType.empty())
               {
-                if (ptConf->m.find("Database Server") != ptConf->m.end() && !ptConf->m["Database Server"]->v.empty())
-                {
-                  if (ptConf->m.find("Database User") != ptConf->m.end() && !ptConf->m["Database User"]->v.empty())
-                  {
-                    list<string> in, out;
-                    string strValue;
-                    stringstream ssQuery;
-                    ServiceJunction junction(strError);
-                    StringManip manip;
-                    junction.setApplication("Warden");
-                    ptData = new Json;
-                    ptData->insert("Service", "mysql");
-                    ptData->insert("User", ptConf->m["Database User"]->v);
-                    ptData->insert("Password", ptConf->m["Database Password"]->v);
-                    ptData->insert("Server", ptConf->m["Database Server"]->v);
-                    ptData->insert("Database", ptConf->m["Database"]->v);
-                    ssQuery << "select * from person where userid = '" << manip.escape(strUser, strValue) << "' and `password` = concat('*',upper(sha1(unhex(sha1('" << manip.escape(strPassword, strValue) << "')))))";
-                    ptData->insert("Query", ssQuery.str());
-                    in.push_back(ptData->json(strJson));
-                    delete ptData;
-                    if (junction.request(in, out, strError))
-                    {
-                      if (!out.empty())
-                      {
-                        Json *ptStatus = new Json(out.front());
-                        if (ptStatus->m.find("Status") != ptStatus->m.end() && ptStatus->m["Status"]->v == "okay")
-                        {
-                          if (out.size() > 1)
-                          {
-                            bProcessed = true;
-                            keys.push_back(strUser);
-                            ptData = new Json;
-                            ptData->insert("_modified", ssCurrent.str(), 'n');
-                            ptData->insert("Password", strPassword);
-                            if (pStorage->add(keys, ptData, strError))
-                            {
-                              bUpdated = true;
-                            }
-                            delete ptData;
-                            keys.clear();
-                          }
-                          else
-                          {
-                            strError = "Failed authentication.";
-                          }
-                        }
-                        else if (ptStatus->m.find("Error") != ptStatus->m.end() && !ptStatus->m["Error"]->v.empty())
-                        {
-                          strError = ptStatus->m["Error"]->v;
-                        }
-                        else
-                        {
-                          strError = "Encountered an unknown error.";
-                        }
-                        delete ptStatus;
-                      }
-                      else
-                      {
-                        strError = "Failed to receive a response.";
-                      }
-                    }
-                    in.clear();
-                    out.clear();
-                  }
-                  else
-                  {
-                    strError = "Please provide the Database User.";
-                  }
-                }
-                else
-                {
-                  strError = "Please provide the Database Server.";
-                }
+                ptData->insert("Type", strType);
               }
-              else
+              ptData->insert("Password", strPassword);
+              if (pStorage->add(keys, ptData, strError))
               {
-                strError = "Please provide the Database Password.";
+                bUpdated = true;
               }
+              delete ptData;
+              keys.clear();
+            }
+            else if (ptStatus->m.find("Error") != ptStatus->m.end() && !ptStatus->m["Error"]->v.empty())
+            {
+              strError = ptStatus->m["Error"]->v;
             }
             else
             {
-              strError = "Please provide the Database.";
+              strError = "Encountered an unknown error.";
             }
-          }
-          delete ptConf;
-          subKeys.clear();
-        }
-      }
-      // }}}
-      // {{{ verify
-      else if (strFunction == "verify")
-      {
-        string strSubError;
-        ptData = new Json;
-        keys.push_back(strApplication);
-        keys.push_back(strUser);
-        if (pStorage->retrieve(keys, ptData, strSubError))
-        {
-          if ((strType.empty() || (ptData->m.find("Type") != ptData->m.end() && ptData->m["Type"]->v == strType)) && ptData->m.find("Password") != ptData->m.end() && ptData->m["Password"]->v == strPassword)
-          {
-            bProcessed = true;
+            delete ptStatus;
           }
           else
           {
-            strError = "Failed authorization.";
+            strError = "Failed to receive a response.";
           }
         }
-        delete ptData;
-        keys.clear();
-        if (!bProcessed)
-        {
-          list<string> in, out;
-          ServiceJunction junction(strError);
-          junction.setApplication("Warden");
-          ptData = new Json;
-          ptData->insert("Service", "password");
-          ptData->insert("Function", "verify");
-          ptData->insert("Application", strApplication);
-          ptData->insert("User", strUser);
-          ptData->insert("Password", strPassword);
-          ptData->insert("Type", strType);
-          in.push_back(ptData->json(strJson));
-          delete ptData;
-          if (junction.request(in, out, strError))
-          {
-            if (!out.empty())
-            {
-              Json *ptStatus = new Json(out.front());
-              if (ptStatus->m.find("Status") != ptStatus->m.end() && ptStatus->m["Status"]->v == "okay")
-              {
-                bProcessed = true;
-                keys.push_back(strApplication);
-                keys.push_back(strUser);
-                ptData = new Json;
-                ptData->insert("_modified", ssCurrent.str(), 'n');
-                if (!strType.empty())
-                {
-                  ptData->insert("Type", strType);
-                }
-                ptData->insert("Password", strPassword);
-                if (pStorage->add(keys, ptData, strError))
-                {
-                  bUpdated = true;
-                }
-                delete ptData;
-                keys.clear();
-              }
-              else if (ptStatus->m.find("Error") != ptStatus->m.end() && !ptStatus->m["Error"]->v.empty())
-              {
-                strError = ptStatus->m["Error"]->v;
-              }
-              else
-              {
-                strError = "Encountered an unknown error.";
-              }
-              delete ptStatus;
-            }
-            else
-            {
-              strError = "Failed to receive a response.";
-            }
-          }
-          in.clear();
-          out.clear();
-        }
+        in.clear();
+        out.clear();
       }
-      // }}}
-      // {{{ invalid
-      else
-      {
-        strError = "Please provide a valid Function:  login, verify.";
-      }
-      // }}}
     }
     else
     {
-      strError = "Please provide the Function.";
+      ptData = new Json;
+      keys.push_back(strUser);
+      if (pStorage->retrieve(keys, ptData, strSubError) && ptData->m.find("Password") != ptData->m.end() && ptData->m["Password"]->v == strPassword)
+      {
+        bProcessed = true;
+      }
+      delete ptData;
+      keys.clear();
+      if (!bProcessed)
+      {
+        list<string> subKeys;
+        Json *ptConf = new Json;
+        Warden warden("Central", strUnix, strError);
+        subKeys.push_back("conf");
+        if (warden.vaultRetrieve(subKeys, ptConf, strError))
+        {
+          if (ptConf->m.find("Database") != ptConf->m.end() && !ptConf->m["Database"]->v.empty())
+          {
+            if (ptConf->m.find("Database Password") != ptConf->m.end() && !ptConf->m["Database Password"]->v.empty())
+            {
+              if (ptConf->m.find("Database Server") != ptConf->m.end() && !ptConf->m["Database Server"]->v.empty())
+              {
+                if (ptConf->m.find("Database User") != ptConf->m.end() && !ptConf->m["Database User"]->v.empty())
+                {
+                  list<string> in, out;
+                  string strValue;
+                  stringstream ssQuery;
+                  ServiceJunction junction(strError);
+                  StringManip manip;
+                  junction.setApplication("Warden");
+                  ptData = new Json;
+                  ptData->insert("Service", "mysql");
+                  ptData->insert("User", ptConf->m["Database User"]->v);
+                  ptData->insert("Password", ptConf->m["Database Password"]->v);
+                  ptData->insert("Server", ptConf->m["Database Server"]->v);
+                  ptData->insert("Database", ptConf->m["Database"]->v);
+                  ssQuery << "select * from person where userid = '" << manip.escape(strUser, strValue) << "' and `password` = concat('*',upper(sha1(unhex(sha1('" << manip.escape(strPassword, strValue) << "')))))";
+                  ptData->insert("Query", ssQuery.str());
+                  in.push_back(ptData->json(strJson));
+                  delete ptData;
+                  if (junction.request(in, out, strError))
+                  {
+                    if (!out.empty())
+                    {
+                      Json *ptStatus = new Json(out.front());
+                      if (ptStatus->m.find("Status") != ptStatus->m.end() && ptStatus->m["Status"]->v == "okay")
+                      {
+                        if (out.size() > 1)
+                        {
+                          bProcessed = true;
+                          keys.push_back(strUser);
+                          ptData = new Json;
+                          ptData->insert("_modified", ssCurrent.str(), 'n');
+                          ptData->insert("Password", strPassword);
+                          if (pStorage->add(keys, ptData, strError))
+                          {
+                            bUpdated = true;
+                          }
+                          delete ptData;
+                          keys.clear();
+                        }
+                        else
+                        {
+                          strError = "Failed authentication.";
+                        }
+                      }
+                      else if (ptStatus->m.find("Error") != ptStatus->m.end() && !ptStatus->m["Error"]->v.empty())
+                      {
+                        strError = ptStatus->m["Error"]->v;
+                      }
+                      else
+                      {
+                        strError = "Encountered an unknown error.";
+                      }
+                      delete ptStatus;
+                    }
+                    else
+                    {
+                      strError = "Failed to receive a response.";
+                    }
+                  }
+                  in.clear();
+                  out.clear();
+                }
+                else
+                {
+                  strError = "Please provide the Database User.";
+                }
+              }
+              else
+              {
+                strError = "Please provide the Database Server.";
+              }
+            }
+            else
+            {
+              strError = "Please provide the Database Password.";
+            }
+          }
+          else
+          {
+            strError = "Please provide the Database.";
+          }
+        }
+        delete ptConf;
+        subKeys.clear();
+      }
     }
   }
   else

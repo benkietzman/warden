434f3258e3c33ef860e2135e4c684f4b862e8fe0
Ben Kietzman <ben@kietzman.org>
2021-04-20 17:47:57 -0500

    Added the authz module.

---
+++ module/authz.cpp
@@ -0,0 +1,176 @@
+// vim600: fdm=marker
+/* -*- c++ -*- */
+///////////////////////////////////////////
+// Authorization
+// -------------------------------------
+// file       : authz.cpp
+// author     : Ben Kietzman
+// begin      : 2021-04-20
+// copyright  : kietzman.org
+// email      : ben@kietzman.org
+///////////////////////////////////////////
+
+/**************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+**************************************************************************/
+
+// {{{ includes
+#include <cerrno>
+#include <cstring>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <sstream>
+#include <string>
+#include <sys/types.h>
+#include <unistd.h>
+using namespace std;
+#include <Json>
+#include <Storage>
+#include <StringManip>
+#include <Warden>
+using namespace common;
+// }}}
+// {{{ main()
+int main(int argc, char *argv[])
+{
+  bool bProcessed = false, bUpdated = false;
+  string strError, strJson, strUnix;
+  stringstream ssMessage;
+  Json *ptJson;
+  Storage *pStorage = new Storage;
+  StringManip manip;
+
+  // {{{ command line arguments
+  for (int i = 1; i < argc; i++)
+  {
+    string strArg = argv[i];
+    if (strArg.size() > 7 && strArg.substr(0, 7) == "--unix=")
+    {
+      strUnix = strArg.substr(7, strArg.size() - 7);
+      manip.purgeChar(strUnix, strUnix, "'");
+      manip.purgeChar(strUnix, strUnix, "\"");
+    }
+  } 
+  // }}}
+  if (getline(cin, strJson))
+  {
+    list<string> keys;
+    string strApplication = "Warden", strSubError, strUser;
+    stringstream ssCurrent;
+    time_t CCurrent;
+    Json *ptData;
+    Warden warden(strApplication, strUnix, strError);
+    time(&CCurrent);
+    ssCurrent << CCurrent;
+    ptJson = new Json(strJson);
+    // {{{ load cache
+    if (ptJson->m.find("_storage") != ptJson->m.end())
+    {
+      pStorage->put(ptJson->m["_storage"]);
+      delete ptJson->m["_storage"];
+      ptJson->m.erase("_storage");
+    }
+    ptData = new Json;
+    if (pStorage->retrieve(keys, ptData, strSubError))
+    {
+      for (map<string, Json *>::iterator i = ptData->m.begin(); i != ptData->m.end(); i++)
+      {
+        stringstream ssModified;
+        time_t CModified;
+        keys.push_back(i->first);
+        if (i->second->m.find("_modified") == i->second->m.end())
+        {
+          i->second->insert("_modified", ssCurrent.str(), 'n');
+          if (pStorage->add(keys, i->second, strSubError))
+          { 
+            bUpdated = true;
+          }
+        }
+        ssModified.str(i->second->m["_modified"]->v);
+        ssModified >> CModified;
+        if ((CCurrent - CModified) > 300 && pStorage->remove(keys, strSubError))
+        { 
+          bUpdated = true;
+        }
+        keys.pop_back();
+      }
+    }
+    delete ptData;
+    keys.clear();
+    // }}}
+    if (ptJson->m.find("User") != ptJson->m.end() && !ptJson->m["User"]->v.empty())
+    {
+      strUser = ptJson->m["User"]->v;
+    }
+    else if (ptJson->m.find("userid") != ptJson->m.end() && !ptJson->m["userid"]->v.empty())
+    {
+      strUser = ptJson->m["userid"]->v;
+    }
+    ptData = new Json;
+    keys.push_back(strUser);
+    if (pStorage->retrieve(keys, ptData, strSubError))
+    {
+      bProcessed = true;
+      if (ptData->m.find("Data") != ptData->m.end())
+      {
+        ptJson->insert("Data", ptData->m["Data"]);
+      }
+    }
+    delete ptData;
+    keys.clear();
+    strSubError.clear();
+    if (!bProcessed)
+    {
+      Json *ptCentral = new Json;
+      if (warden.central(strUser, ptCentral, strSubError))
+      {
+        bProcessed = true;
+        keys.push_back(strUser);
+        ptData = new Json;
+        ptData->insert("_modified", ssCurrent.str(), 'n');
+        ptData->insert("Data", ptCentral);
+        ptJson->insert("Data", ptCentral);
+        if (pStorage->add(keys, ptData, strError))
+        {
+          bUpdated = true;
+        }
+        delete ptData;
+        keys.clear();
+      }
+      else
+      {
+        strError = strSubError;
+      }
+      delete ptCentral;
+    }
+  }
+  else
+  {
+    ptJson = new Json;
+    strError = "Please provide the request.";
+  }
+  ptJson->insert("Status", ((bProcessed)?"okay":"error"));
+  if (!strError.empty())
+  {
+    ptJson->insert("Error", strError);
+  }
+  if (bUpdated)
+  {
+    pStorage->lock();
+    ptJson->insert("_storage", pStorage->ptr());
+    pStorage->unlock();
+  }
+  cout << ptJson << endl;
+  delete ptJson;
+  delete pStorage;
+
+  return 0;
+}
+// }}}
